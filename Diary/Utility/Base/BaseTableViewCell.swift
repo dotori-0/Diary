3dd0f821871d122455c5755fe986f924c063fcd0
dotori-0 <sesan131@gmail.com>
2022-08-24 21:50:50 +0900

    [Add] Base Table View Cell

--- Diary/Utility/Base/BaseTableViewCell.swift
+++ Diary/Utility/Base/BaseTableViewCell.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+import SnapKit
+
+
 class BaseTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
