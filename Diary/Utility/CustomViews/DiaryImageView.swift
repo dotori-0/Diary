f76913f0053ca7ce1c1765e14490d8a45aa8160e
dotori-0 <sesan131@gmail.com>
2022-08-24 21:50:22 +0900

    [Add] Custom Image View

---
+++ Diary/Utility/CustomViews/DiaryImageView.swift
@@ -0,0 +1,25 @@
+//
+//  DiaryImageView.swift
+//  Diary
+//
+//  Created by SC on 2022/08/24.
+//
+
+import UIKit
+
+class DiaryImageView: UIImageView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        contentMode = .scaleAspectFill
+        layer.cornerRadius = 8
+        layer.borderWidth = 1
+        layer.borderColor = Constants.Color.tintColor.cgColor
+    }
+}
